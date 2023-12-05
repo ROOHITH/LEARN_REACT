4c7c6a497c13de0dfe1d7c1127d050b258fa7ebd
ROOHITH <rohith602@outlook.com>
2023-12-05 12:55:32 +0530

    commit

--- src/components/UseEffectDemo.jsx
+++ src/components/UseEffectDemo.jsx
@@ -1,12 +1,43 @@
-import { useState,useEffect } from "react";
-export default function UseEffectDemo(){
-    const[name,setName]=useState([]);
+import { useState, useEffect } from "react";
+export default function UseEffectDemo() {
+  const [name, setName] = useState([]);
 
+  useEffect(() => {
     fetch("/names.json")
-    .then(response=>response.json())
-    .then((data)=>setName(data));
+      .then((response) => response.json())
+      .then((data) => setName(data));
+  }, []);
 
-    return(<>
-    <h4>{name.join(",")}</h4>
-    </>);
+  const [selectedNames, setSelectedNames] = useState(null);
+  const [selectedNamesDetails, setSelectedNamesDetails] = useState(null);
+
+  useEffect(() => {
+    if (selectedNames) {
+      fetch(`/${selectedNames}.json`)
+        .then((response) => response.json())
+        .then((data) => setSelectedNamesDetails(data));
+    }
+  }, [selectedNames]);
+
+  return (
+    <>
+      <h4>
+        Names:{" "}
+        {name.map((data, index) => {
+          return (
+            <button
+              key={index}
+              onClick={(e) => {
+                setSelectedNames(data);
+              }}
+            >
+              {data}
+            </button>
+          );
+        })}
+      </h4>
+
+      <div>{JSON.stringify(selectedNamesDetails)}</div>
+    </>
+  );
 }
